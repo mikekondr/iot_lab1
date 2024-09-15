768a2726f36f15ba004eddfe200b5d5487228ee2
mikekondr <mike.kondr@gmail.com>
2024-09-15 22:07:52 +0300

    my improvements

--- app/src/main/java/top/mk85/mike/guessthenumber/MainActivity.kt
+++ app/src/main/java/top/mk85/mike/guessthenumber/MainActivity.kt
@@ -1,7 +1,10 @@
 package top.mk85.mike.guessthenumber
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -10,14 +13,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var tvInfo: TextView
     private lateinit var etInput: EditText
     private lateinit var bControl: Button
+    private lateinit var tvTries: TextView
 
     private var guess: Int = 0
     private var gameFinished: Boolean = false
+    private var tries: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,18 +38,38 @@ class MainActivity : AppCompatActivity() {
         tvInfo = findViewById(R.id.textView)
         etInput = findViewById(R.id.editTextNumberDecimal)
         bControl = findViewById(R.id.button)
+        tvTries = findViewById(R.id.textView3)
 
         guess = (Math.random() * 100).toInt()
+
+        tvTries.text = String.format(resources.getString(R.string.tries), tries.toString())
+
+        etInput.setOnEditorActionListener { v: TextView?, actionId: Int, event: KeyEvent? ->
+            var handled = false
+            if (actionId == EditorInfo.IME_ACTION_SEND) {
+                getResult(this.currentFocus)
+                handled = true
+            }
+            handled
+        }
     }
 
     fun onClick(v:View) {
-        if (!gameFinished) {
-            val inp: Int = etInput.text.toString().toInt()
+        getResult(v)
+    }
 
-            if (inp > guess)
+    private fun getResult(v:View?) {
+        val tInp: String = etInput.text.toString()
+        if (!gameFinished && tInp.isEmpty()) return
+
+        if (!gameFinished) {
+            val inp: Int = tInp.toInt()
+
+            if (inp > guess) {
                 tvInfo.text = resources.getString(R.string.ahead)
-            else if (inp < guess)
+            } else if (inp < guess) {
                 tvInfo.text = resources.getString(R.string.behind)
+            }
             else {
                 tvInfo.text = resources.getString(R.string.hit)
                 bControl.text = resources.getString(R.string.play_more)
@@ -54,8 +80,26 @@ class MainActivity : AppCompatActivity() {
             bControl.text = resources.getString(R.string.input_value)
             tvInfo.text = resources.getText(R.string.try_to_guess)
             gameFinished = false
+            tries = 0
         }
 
+        if (gameFinished) {
+            tvTries.visibility = View.INVISIBLE
+            etInput.visibility = View.INVISIBLE
+            if (v != null) {
+                val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(v.windowToken, 0)
+            }
+        } else {
+            tries++
+            tvTries.visibility = View.VISIBLE
+            tvTries.text = String.format(resources.getString(R.string.tries), tries.toString())
+
+            etInput.visibility = View.VISIBLE
+            etInput.requestFocus()
+            val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(etInput, InputMethodManager.SHOW_IMPLICIT)
+        }
         etInput.setText("")
     }
 }
