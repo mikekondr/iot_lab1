ec1ac687d9b521296d26479188f222f57743248e
mikekondr <mike.kondr@gmail.com>
2024-09-15 20:51:15 +0300

    lab example

--- app/src/main/java/top/mk85/mike/guessthenumber/MainActivity.kt
+++ app/src/main/java/top/mk85/mike/guessthenumber/MainActivity.kt
@@ -1,12 +1,24 @@
 package top.mk85.mike.guessthenumber
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tvInfo: TextView
+    private lateinit var etInput: EditText
+    private lateinit var bControl: Button
+
+    private var guess: Int = 0
+    private var gameFinished: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +28,34 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        tvInfo = findViewById(R.id.textView)
+        etInput = findViewById(R.id.editTextNumberDecimal)
+        bControl = findViewById(R.id.button)
+
+        guess = (Math.random() * 100).toInt()
+    }
+
+    fun onClick(v:View) {
+        if (!gameFinished) {
+            val inp: Int = etInput.text.toString().toInt()
+
+            if (inp > guess)
+                tvInfo.text = resources.getString(R.string.ahead)
+            else if (inp < guess)
+                tvInfo.text = resources.getString(R.string.behind)
+            else {
+                tvInfo.text = resources.getString(R.string.hit)
+                bControl.text = resources.getString(R.string.play_more)
+                gameFinished = true
+            }
+        } else {
+            guess = (Math.random() * 100).toInt()
+            bControl.text = resources.getString(R.string.input_value)
+            tvInfo.text = resources.getText(R.string.try_to_guess)
+            gameFinished = false
+        }
+
+        etInput.setText("")
     }
 }
